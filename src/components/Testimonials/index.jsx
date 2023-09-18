15516318df33274bcfa23f3d28c58e2e8cbe0c92
DevvSakib <devvsakib@gmail.com>
2023-09-18 15:35:33 +0600

    about page done

---
+++ src/components/Testimonials/index.jsx
@@ -0,0 +1,47 @@
+import { data } from '../../data/data';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import "./style.css"
+
+const Testimonials = () => {
+    const options = {
+        loop: true,
+        center: true,
+        items: 2,
+        margin: 30,
+        autoplay: true,
+        dots: false,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            }
+        }
+    };
+    return (
+        <OwlCarousel id="customer-testimonoals" className="owl-carousel owl-theme" {...options}>
+            {
+                data?.testimonials?.map((item, idx) => (
+                    <div key={idx} className="item  mx-auto grid gap-5 w-auto md:w-[500px] bg-white p-6 px-10 rounded-2xl shadow-lg duration-300 ease-linear pb-10">
+                        <img className="w-[50px]" src="/img/icons/quotation-mark.svg" alt="quote" />
+                        <p>
+                            {item?.testimonial}
+                        </p>
+                        <div>
+                            <h4 className='font-semibold text-lg'>{item?.author}</h4>
+                            <span className='text-gray'>{item?.role}</span>
+                        </div>
+                    </div>
+                ))
+            }
+        </OwlCarousel>
+    )
+}
+
+export default Testimonials;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 import Layout from '../components/Layout'
 import { Helmet } from 'react-helmet'
 import { seo } from '../meta/aboutSEO'
-import Testimonial from '../components/Testimonials'
 import HeroSection from '../components/Common/HeroSection'
+import Testimonials from '../components/Testimonials'
 
 const About = () => {
     return (
@@ -15,7 +15,15 @@ const About = () => {
             </Helmet>
             <HeroSection />
             <Layout>
-                <Testimonial />
+                <div className='mb-10'>
+                    <div className='text-center'>
+                        <h3 className="text-3xl font-circularBol">Testimonials</h3>
+                        <p className="text-gray mt-2">Few words from our customers.</p>
+                    </div>
+                    <div className='w-full pb-10'>
+                        <Testimonials />
+                    </div>
+                </div>
             </Layout>
         </>
     )
