ec27dcc8b9282557b42d13a195d9cad23abef80d
DevvSakib <devvsakib@gmail.com>
2023-09-20 10:28:44 +0600

    image res and some minor changes

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -19,13 +19,13 @@ const Services = () => {
                         {
                             data.services.map((item, index) => (
                                 <Link to={item.path} className="flex flex-col justify-between items-center border border-gray/20 p-5 rounded-2xl shadow-sm hover:shadow-2xl duration-300 ease-linear" key={index}>
-                                    <div className="w-full md:w-1/2">
-                                        <img src={item.thumbnail} alt={item.title} className="w-full" />
+                                    <div className="w-full mx-auto md:ml-0 md:w-[100px] md:h-[80px]">
+                                        <img src={item.thumbnail} alt={item.title} className="" />
                                     </div>
-                                    <div className="">
-                                        <h3 className="text-xl md:text-3xl font-circularBol">{item.heading}</h3>
+                                    <div className="mt-5">
+                                        <h3 className="text-xl md:text-3xl font-circularBol">{item.heading == "Application Development and Modernization" ? "Application Modernization" : item.heading}</h3>
                                         <p className="text-gray mt-2">
-                                            {item.description}
+                                            {item.description.slice(0, 150) + '... Read More'}
                                         </p>
                                     </div>
                                 </Link>
