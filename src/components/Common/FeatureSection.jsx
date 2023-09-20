b1865f6d11fc69605de66cac1321a72ed13ef2ab
DevvSakib <devvsakib@gmail.com>
2023-09-20 15:53:58 +0600

    home page hero and feature sec done

---
+++ src/components/Common/FeatureSection.jsx
@@ -0,0 +1,37 @@
+const FeatureSection = ({ list }) => {
+    return (
+        <section>
+            <div className='mb-10 mt-16'>
+                <div className='w-full pb-10'>
+                    <div className='grid md:grid-cols-3 items- justify-center '>
+                        <div className='w-full md:w-1/2 mb-5 '>
+                            <img src='/img/icons/shunyeka-color.svg' alt='about us' className='w-full' />
+                        </div>
+                        <div className='col-span-2'>
+                            <div className=' mb-5'>
+                                <h4 className='text-4xl font-circularBol mb-3'> Cracking the Code of Digital Evolution </h4>
+                                <p className='text-gray'> At ShunyEka, we are invested in enabling businesses to seamlessly combine operations and development environments, thereby automating core processes so that you can focus on what matters the most – innovation and expansion. </p>
+                            </div>
+                            <div>
+                                <ul className='grid gap-5'>
+                                    {
+                                        list.map(item => (
+                                            <li className='flex gap-5 items-start'>
+                                                <img src="/img/icons/arrow-correct.svg" alt="" />
+                                                <p>
+                                                    {item}
+                                                </p>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default FeatureSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,74 @@
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
+import FeatureSection from "../components/Common/FeatureSection";
 import { seo } from "../meta/aboutSEO";
+const list = [
+    "Visibility: Experience the power of cloud orchestration for data monitoring and real-time analytics",
+    "Strength: Enhance decision-making potential through advanced AI, Machine Learning, and IOT frameworks",
+    "Security: Integrate and automate security checks seamlessly into development cycle for increased functional efficiency"
+]
+
 const Home = () => {
     return (
         <>
-            <meta name="title" content={seo.title} />
+            <section className="about-hero relative py-32">
+                <Layout>
+                    <div className="grid md:grid-cols-2 gap-10">
+                        {/* left content*/}
+                        <div className="relative">
+                            <h3 className="text-6xl font-circularBol leading-snug text-black"> Your Automation Partner in the Brave New World of Cloud </h3>
+                            <div className="text-gray">
+                                <p> The age of ZeroTech solutions is at hand, zero wastage, complete sustainability, zero frailty, complete stability. ShunyEka’s unique service offerings help you follow a zero compromise path towards business growth in the digital world.</p>
+                                {/* <p> Your way in the new cloud environment, managing the various intricacies and complexities within it, and scaling up processes like data monitoring and application deployment can be daunting without the right support. Our team of DevXOps experts help you at each step of the way, from cloud architecture design for serverless applications to integration of new technological frontiers such as IOT.</p> */}
+                            </div>
+                            <div className=" mt-10">
+                                <Link to="/contact-us" className="bg-primary text-white rounded-md px-10 py-5">Get In Touch</Link>
+                            </div>
 
+                        </div>
+                        {/* right content */}
+                        <div className="grid md:grid-cols-2 gap-5 md:gap-10 moving text-3xl min-h-[500px]">
+                            <div className="bg-white rounded-lg grid place-items-center text-center p-5 mt-10">
+                                <div className="service-box grid justify-center items-center">
+                                    <img src="/img/icons/cloud-computing.svg" alt="icons" className="max-w-[100%] mx-auto" />
+                                    <h5>
+                                        Cloud Automation
+                                    </h5>
+                                </div>
+                            </div>
+                            <div className="bg-white rounded-lg grid place-items-center text-center p-5">
+                                <div className="service-box grid justify-center items-center">
+                                    <img src="/img/icons/cloud-computing2.svg" alt="icons" className="max-w-[100%] mx-auto" />
+                                    <h5>
+                                        Cloud Security
+                                    </h5>
+                                </div>
+                            </div>
+                            <div className="bg-white rounded-lg grid place-items-center text-center p-5">
+                                <div className="service-box grid justify-center items-center">
+                                    <img src="/img/svg/Application Modernization.svg" alt="icons" className="max-w-[100%] mx-auto" />
+                                    <h5>
+                                        Application Modernization
+                                    </h5>
+                                </div>
+                            </div>
+                            <div className="bg-white rounded-lg grid place-items-center text-center p-5 mb-10">
+                                <div className="service-box grid justify-center items-center">
+                                    <img src="/img/svg/Serverless Application.svg" alt="icons" className="max-w-[100%] mx-auto" />
+                                    <h5>
+                                        Serverless Application
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Layout>
+            </section>
             <Layout>
-                <section className="h-screen bg-orange-300">
-                </section>
-                <section className="h-screen bg-orange-400">
-                </section>
+                <FeatureSection list={list} />
                 <section className="h-screen bg-orange-500">
                 </section>
-            </Layout>
+            </Layout >
         </>
     )
 }
